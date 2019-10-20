ef64a9d856147b6e73dd94591227c85e539cca68
unknown <al221874203@gmail.com>
2019-10-19 22:51:36 -0500

    datatables funcional

--- resources/views/menuplatillos/index.blade.php
+++ resources/views/menuplatillos/index.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <title>Menu Platillos</title>
-    <link rel="stylesheet" href="datatables/bootstrap.css">
+    <link rel="stylesheet" href="{{ asset('datatables/bootstrap.css')}}">
     <link rel="stylesheet" href="{{{ asset('datatables/bootstrap4.min.css')}}}">
     <link rel="stylesheet" href="{{{ asset('datatables/css/jquery.dataTables.min.css')}}}">
     <link rel="stylesheet" href="{{{ asset('datatables/css/estilos.css')}}}">
@@ -50,6 +50,91 @@
         </tbody>
           </table>
           </div>
+
+<script src="{{{ asset('datatables/js/jquery-3.3.1.js')}}}"></script>
+<script src="{{{ asset('datatables/js/jquery.dataTables.min.js')}}}"></script>
+<script src="{{{ asset('datatables/js/dataTables.bootstrap4.min.js')}}}"></script>
+<script src="{{{ asset('datatables/js/dataTables.buttons.min.js')}}}"></script>
+<script src="{{{ asset('datatables/js/jszip.min.js')}}}"></script>
+<script src="{{{ asset('datatables/js/pdfmake.min.js')}}}"></script>
+<script src="{{{ asset('datatables/js/vfs_fonts.js')}}}"></script>
+<script src="{{{ asset('datatables/js/buttons.html5.min.js')}}}"></script>
+<script src="{{{ asset('datatables/js/buttons.print.min.js')}}}"></script>
+<script src="{{{ asset('datatables/js/buttons.print.min.js')}}}"></script>
+<script src="{{{ asset('datatables/js/buttons.colVis.min.js')}}}"></script>
+
+
+
+<script>
+$(document).ready(function() {
+    $('#a1').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'copyHtml5',
+                exportOptions: {
+                    columns: [ 0,':visible' ]
+                }
+            },
+            {
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: ':visible'
+                }
+            },
+
+            {
+                extend: 'pdfHtml5',
+                exportOptions: {
+                    columns: ':visible'
+                }
+            },
+            {
+                extend: 'print',
+                exportOptions: {
+                    columns: [0, ':visible' ]
+                }
+            },
+            'colvis'
+        ],
+        "language": {
+            "decimal": "",
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Entradas",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        },
+        "buttons":{
+            "copy" : 'Copiar',
+            "csv" : 'Exportar a CSV',
+            "print" : 'Imprimir',
+            copyTitle: 'Los datos fueron copiados',
+            copySuccess: {
+                _: '%d lineas copiadas',
+                1: '1 línea copiada'
+        },
+            "colvis" : 'Filtrar Columnas'
+
+        }
+
+        }
+    } );
+} );
+
+
+</script>
 
 </body>
 
