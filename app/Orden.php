4bcea642b9f97ad890c16e2c711afbb0ae5f058b
Victor <victor.rivas.1999@gmail.com>
2019-11-30 02:57:11 -0600

    MODELOS CON BULDIER (BELONGSTO HASMANY)

--- app/Orden.php
+++ app/Orden.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Orden extends Model
 {
-    //
+    public function user(){
+    	return	$this->belongsTo(User::class);
+    }
+    public function detalle_ordens(){
+    	return	$this->hasMany(MenuPlatillo::class);
+    }
 }
